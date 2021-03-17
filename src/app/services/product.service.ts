152b084148cc7d4e28d35b356458259b78b9b28e
mcaliskan7 <merve.caliskan@std.yeditepe.edu.tr>
2021-03-18 00:15:03 +0300

    March 18, 2021

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  apiUrl = "https://localhost:44332/api/";
+
+  constructor(private httpClient:HttpClient) { }
+
+  getProducts():Observable<ListResponseModel<Product>>{
+    let newPath = this.apiUrl + "products/getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);    
+  }
+  
+  getProductsByCategoryId(categoryId:number):Observable<ListResponseModel<Product>>{
+    let newPath = this.apiUrl + "products/getbycategoryid?categoryId="+categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);    
+  } 
+}
